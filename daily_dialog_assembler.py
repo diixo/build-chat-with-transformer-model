d5b8442c5b58b9a886a4d59b204a9b334229b048
diixo <anatolii.podoba@gmail.com>
2026-03-07 18:22:31 +0200

    daily_dialog_assembler.py filtering stage-7 : clean-up lite

--- daily_dialog_assembler.py
+++ daily_dialog_assembler.py
@@ -99,9 +99,10 @@ def daily_dialog_expanded_gen_filter():
     #overwrrite the original file with filtered lines:
     with open(Path(file_path), "w", encoding="utf-8") as fout:
         for line in lines:
-            line = line.replace("User:,", "User: ")
-            line = line.replace(".,!", ".")
-            line = line.replace("User:!", "User:")
+            line = line.replace(",!", "!")
+
+            if line.strip() == "User:":
+                line = "User: maybe\n"
             fout.write(line)
 
 
